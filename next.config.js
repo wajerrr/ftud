7bd42fff315382d41f1730033fa6175be1fef0b0
Przemek Wajerowicz <przemek.wajerowicz@abcam.com>
2023-09-24 18:39:07 +0100

    deploy pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 const nextConfig = {
   output: 'export',
 
@@ -12,6 +24,8 @@ const nextConfig = {
 
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
+  assetPrefix,
+  basePath,
   images: {
     unoptimized: true,
   },
