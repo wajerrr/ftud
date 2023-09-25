cfb58aca09bdf43064f7a1dec42138dee9bd9802
Przemek Wajerowicz <przemek.wajerowicz@abcam.com>
2023-09-25 12:46:48 +0100

    fix

--- lib/img-loader.ts
+++ lib/img-loader.ts
@@ -1,10 +1,4 @@
 export default function myImageLoader({ src, width, quality }) {
-  const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-  let imgPrefix = '';
-  if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-    imgPrefix = `/${repo}`;
-  }
-  return `${imgPrefix}${src}`;
+  return `/ftud${src}`;
 }
