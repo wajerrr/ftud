a6619d56445bf1c350ea5f7d508938f677068bbe
Przemek Wajerowicz <przemek.wajerowicz@abcam.com>
2023-10-17 15:07:42 +0100

    added goat counter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
+      <script
+        data-goatcounter="https://ftud.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      ></script>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
